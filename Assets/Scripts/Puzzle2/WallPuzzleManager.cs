92e834296a384c9c0209cf3870dbfe8ff4955410
nouranragy <nouranragy341@gmail.com>
2026-09-15 17:44:57 +0300

    fix main chest unlock state and past wall puzzle visibility

--- Assets/Scripts/Puzzle2/WallPuzzleManager.cs
+++ Assets/Scripts/Puzzle2/WallPuzzleManager.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using Photon.Pun;
 using ExitGames.Client.Photon;
+using UnityEditor.SceneManagement;
 
 public class WallPuzzleManager : MonoBehaviourPunCallbacks
 {
     public static WallPuzzleManager Instance;
+
+    public GameObject wallPuzzleParentContainer;
     public WallSlot wallslot1;
     public WallSlot wallslot2;
     public GameObject[] gearObjects;
@@ -15,8 +18,24 @@ public class WallPuzzleManager : MonoBehaviourPunCallbacks
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
     }
+     public override void OnEnable()
+    {
+        base.OnEnable();
+        CheckChestUnlockedStatus();
+        CheckExistingPuzzleState();
+    }
+    public override void OnDisable ()
+    {
+        base.OnDisable();
+    }
+
     private void Start()
     {
+        if(wallPuzzleParentContainer != null)
+        {
+            wallPuzzleParentContainer.SetActive(false);
+        }
+        CheckChestUnlockedStatus();
         CheckExistingPuzzleState();
     }
     public void CheckPuzzleCompletion()
@@ -29,10 +48,42 @@ public class WallPuzzleManager : MonoBehaviourPunCallbacks
     }
     public override void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
     {
+        Debug.Log($"<color=yellow>[WallPuzzleManager] Room Properties Changed! Checking keys...</color>");
+        if (propertiesThatChanged.ContainsKey(MainChest.CHEST_UNLOCKED_KEY))
+        {
+            Debug.Log("<color=green>[WallPuzzleManager] CHEST_UNLOCKED_KEY received from Network!</color>");
+            CheckChestUnlockedStatus();
+        }
         if (propertiesThatChanged.ContainsKey(PUZZLE_KEY))
         {
             CheckExistingPuzzleState();
         }
+    }
+
+    private void CheckChestUnlockedStatus()
+    {
+        if(PhotonNetwork.IsConnectedAndReady && PhotonNetwork.InRoom && PhotonNetwork.CurrentRoom != null ){
+        if(PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue(MainChest.CHEST_UNLOCKED_KEY, out object isUnlocked))
+        {
+            Debug.Log($"[WallPuzzleManager] Value of Chest Unlocked is: {isUnlocked}");
+            if((bool)isUnlocked && wallPuzzleParentContainer != null)
+            {
+                wallPuzzleParentContainer.SetActive(true);
+                Debug.Log($"[WallPuzzleManager] Value of Chest Unlocked is: {isUnlocked}");
+            }  
+        
+           
+        }
+         else
+        {
+            Debug.LogWarning("[WallPuzzleManager] Key 'MainChestUnlocked' was not found in Room Properties!");
+        }  
+    }
+        
+        else
+    {
+        Debug.LogError("[WallPuzzleManager] PhotonNetwork.CurrentRoom is NULL! Not connected to room?");
+    }
     }
     private void CheckExistingPuzzleState()
     {
