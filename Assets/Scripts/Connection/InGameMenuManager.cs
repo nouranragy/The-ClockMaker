ac51dd16adc417b05a71df0be8c9d10d6b0db7bd
nouranragy <nouranragy341@gmail.com>
2026-09-19 10:41:02 +0300

    edit

--- Assets/Scripts/Connection/InGameMenuManager.cs
+++ Assets/Scripts/Connection/InGameMenuManager.cs
@@ -29,38 +29,22 @@ public class InGameMenuManager : MonoBehaviourPunCallbacks
 
     private void Start()
     {
-        // isLeaving = false;
-        // if (settingsPanel) settingsPanel.SetActive(false);
-
-        // // Initialize Sliders to match current AudioSource volumes
-        // if (musicSource != null && musicSlider != null)
-        // {
-        //     musicSlider.value = musicSource.volume;
-        //     musicSlider.onValueChanged.AddListener(OnMusicVolumeChanged);
-        // }
-
-        // if (sfxSource != null && sfxSlider != null)
-        // {
-        //     sfxSlider.value = sfxSource.volume;
-        //     sfxSlider.onValueChanged.AddListener(OnSFXVolumeChanged);
-        // }
         isLeaving = false;
     if (settingsPanel) settingsPanel.SetActive(false);
 
-    // 1. ربط صوت الباك جراوند (لو محطوط مباشرة على GameObject في السين)
+    
     if (musicSource == null)
     {
-        // هيجيب أول AudioSource موجود في المشهد للباك جراوند
+       
         musicSource = FindFirstObjectByType<AudioSource>();
     }
 
-    // 2. ربط صوت SFX مع SoundManager
+   
     if (SoundManager.Instance != null && SoundManager.Instance.sfxSource != null) //music
     {
         sfxSource = SoundManager.Instance.sfxSource;
     }
 
-    // 3. ضبط السليدرز
     if (musicSource != null && musicSlider != null)
     {
         musicSlider.value = musicSource.volume;
@@ -100,21 +84,21 @@ public class InGameMenuManager : MonoBehaviourPunCallbacks
 
     public void OnBackToLobbyButtonClicked()
     {
-     // 1. حماية ضد الضغط المتكرر على الزرار
+    
     if (isLeaving) return;
     isLeaving = true;
 
-    // 2. ضمان عدم وجود خروج نهائي للعبة
+   
     UIController.IsExitingGame = false;
 
-    // 3. لو مش جوة غرفة، ارجع للوبي فوراً
+    
     if (!PhotonNetwork.InRoom)
     {
         LoadLobbyScene();
         return;
     }
 
-    // 4. لو هو الـ Master Client، أعد فتح الغرفة وتصفير حالة اللعبة
+   
     if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom != null)
     {
         PhotonNetwork.CurrentRoom.SetCustomProperties(new ExitGamesHashtable { { GAME_STARTED, false } });
@@ -122,12 +106,11 @@ public class InGameMenuManager : MonoBehaviourPunCallbacks
         PhotonNetwork.CurrentRoom.IsVisible = true;
     }
 
-    // 5. إيقاف أي كوروتاينز وتفريغ الـ RPCs المعلقة للـ LocalPlayer
     StopAllCoroutines();
     PhotonNetwork.RemoveRPCs(PhotonNetwork.LocalPlayer);
 
     StartCoroutine(SafetyLobbyTimeout());
-    // 6. مغادرة الغرفة (Photon هينادي OnLeftRoom أوتوماتيكياً)
+    
     PhotonNetwork.LeaveRoom();
     }
 
@@ -159,7 +142,7 @@ public class InGameMenuManager : MonoBehaviourPunCallbacks
     public override void OnDisconnected(DisconnectCause cause)
 {
     isLeaving = false;
-    // لو فصل الشبكة وهو بيحاول يخرج للوبي، يحمل السين برضه
+    
     if (SceneManager.GetActiveScene().name != lobbySceneName)
     {
         LoadLobbyScene();
@@ -178,53 +161,6 @@ public class InGameMenuManager : MonoBehaviourPunCallbacks
         }
     }
 
-    // public void OnSwitchButtonClicked()
-    // {
-    //     if (!PhotonNetwork.InRoom)
-    //     {
-    //         string currentLocal = SceneManager.GetActiveScene().name;
-    //         string targetLocal = (currentLocal.Equals(masterSceneName, System.StringComparison.OrdinalIgnoreCase))
-    //             ? clientSceneName : masterSceneName;
-
-    //         if (CurtainTransition.Instance != null)
-    //             CurtainTransition.Instance.LoadScene(targetLocal);
-    //         else
-    //             SceneManager.LoadScene(targetLocal);
-
-    //         return;
-    //     }
-
-    //     if (photonView == null)
-    //     {
-    //         Debug.LogError($"[{nameof(InGameMenuManager)}] No PhotonView found on this GameObject; cannot send RPC_ExecuteSceneSwap.");
-    //         return;
-    //     }
-
-    //     string senderCurrentScene = SceneManager.GetActiveScene().name;
-    //     bool isSenderInMaster = senderCurrentScene.Equals(masterSceneName, System.StringComparison.OrdinalIgnoreCase);
-    //     string senderTargetScene = isSenderInMaster ? clientSceneName : masterSceneName;
-    //     string otherTargetScene = isSenderInMaster ? masterSceneName : clientSceneName;
-    //     photonView.RPC(nameof(RPC_ExecuteSceneSwap), RpcTarget.All, PhotonNetwork.LocalPlayer.ActorNumber, senderTargetScene, otherTargetScene);
-    // }
-
-    // [PunRPC]
-    // private void RPC_ExecuteSceneSwap(int senderActorNumber, string senderTargetScene, string otherTargetScene)
-    // {
-    //     string finalTarget = (PhotonNetwork.LocalPlayer.ActorNumber == senderActorNumber)
-    //         ? senderTargetScene
-    //         : otherTargetScene;
-
-    //         ExitGamesHashtable customProps = new ExitGamesHashtable
-    //      {
-    //     { "CurrentScene", finalTarget }
-    //     };
-    //      PhotonNetwork.LocalPlayer.SetCustomProperties(customProps);
-
-    //     if (CurtainTransition.Instance != null)
-    //         CurtainTransition.Instance.LoadScene(finalTarget);
-    //     else
-    //         SceneManager.LoadScene(finalTarget);
-    // }
 
     public void OnSwitchButtonClicked()
 {
@@ -240,7 +176,7 @@ public class InGameMenuManager : MonoBehaviourPunCallbacks
         return;
     }
 
-    // إرسال أمر التبديل لجميع اللاعبين في الغرفة
+   
     photonView.RPC(nameof(RPC_ExecuteSceneSwap), RpcTarget.All);
 }
 
@@ -254,19 +190,19 @@ private void SwapLocalScene()
 {
     string currentScene = SceneManager.GetActiveScene().name;
     
-    // إذا كان اللاعب في Past يذهب إلى Present، والعكس
+   
     string targetScene = currentScene.Equals(masterSceneName, System.StringComparison.OrdinalIgnoreCase) 
         ? clientSceneName 
         : masterSceneName;
 
-    // حفظ المشهد الجديد في خواص اللاعب لتزامن الفوتون
+   
     ExitGamesHashtable customProps = new ExitGamesHashtable
     {
         { "CurrentScene", targetScene }
     };
     PhotonNetwork.LocalPlayer.SetCustomProperties(customProps);
 
-    // تحميل السين
+    
     if (CurtainTransition.Instance != null)
         CurtainTransition.Instance.LoadScene(targetScene);
     else
